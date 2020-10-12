1f8991e0a61a9b2e0ee939c5b550c9960a7e889d
Y.D.C <45694174+CY-1@users.noreply.github.com>
2020-10-12 10:33:49 +0800

    Update 530. 二叉搜索树的最小绝对差.py

--- 530. 二叉搜索树的最小绝对差.py
+++ 530. 二叉搜索树的最小绝对差.py
@@ -23,3 +23,31 @@ class Solution:
         for i in range(1,len(result)):
             a=min(a,result[i]-result[i-1])
         return a
+
+    
+    
+    # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def getMinimumDifference(self, root: TreeNode) -> int:
+
+        result=[]
+        stack=[root]
+        while stack:
+            a=stack.pop()
+            result.append(a.val)
+            if a.left!=None: stack.append(a.left)
+            if a.right!=None: stack.append(a.right)
+
+
+        result.sort()
+        a=float('Inf')
+
+        for i in range(1,len(result)):
+            a=min(a,result[i]-result[i-1])
+        return a
