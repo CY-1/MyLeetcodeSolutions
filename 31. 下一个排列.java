d432e6f6abdac021b356ef43678ff3b386b24ee0
Y.D.C <45694174+CY-1@users.noreply.github.com>
2020-11-10 10:06:38 +0800

    Create 31. 下一个排列.java

---
+++ 31. 下一个排列.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+        int firstIndex = -1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                firstIndex = i;
+                break;
+            }
+        }
+        if (firstIndex == -1) {
+            reverse(nums, 0, nums.length - 1);
+            return;
+        }
+        int secondIndex = -1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] > nums[firstIndex]) {
+                secondIndex = i;
+                break;
+            }
+        }
+        swap(nums, firstIndex, secondIndex);
+        reverse(nums, firstIndex + 1, nums.length - 1);
+        return;
+
+    }
+
+    private void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            swap(nums, i++, j--);
+        }
+    }
+
+    private void swap(int[] nums, int i, int i1) {
+        int tmp = nums[i];
+        nums[i] = nums[i1];
+        nums[i1] = tmp;
+    }
+}
