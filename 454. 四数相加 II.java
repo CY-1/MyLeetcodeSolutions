26f67ad80e4010146ffa7eaf83bfe2c02b8393b8
Y.D.C <45694174+CY-1@users.noreply.github.com>
2020-11-10 10:15:54 +0800

    Create 454. 四数相加 II.java

---
+++ 454. 四数相加 II.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        Map<Integer, Integer> map = new HashMap<>();
+        //Map<Integer, Integer> map = new HashMap<>();
+        int res = 0;
+        for(int i = 0;i<A.length;i++){
+            for(int j= 0;j<B.length;j++){
+                int sumAB = A[i]+B[j];
+                if(map.containsKey(sumAB)) map.put(sumAB,map.get(sumAB)+1);
+                else map.put(sumAB,1);
+            }
+        }
+
+        for(int i = 0;i<C.length;i++){
+            for(int j = 0;j<D.length;j++){
+                int sumCD = -(C[i]+D[j]);
+                if(map.containsKey(sumCD)) res += map.get(sumCD);
+            }
+        }
+        return res;
+    }
+}
