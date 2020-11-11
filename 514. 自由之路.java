6c29498fd6fd6ea61da347fa581dc77eac233192
Y.D.C <45694174+CY-1@users.noreply.github.com>
2020-11-11 14:34:52 +0800

    Create 514. 自由之路.java

---
+++ 514. 自由之路.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int findRotateSteps(String ring, String key) {
+        int n = ring.length(), m = key.length();
+        List<Integer>[] pos = new List[26];
+        for (int i = 0; i < 26; ++i) {
+            pos[i] = new ArrayList<Integer>();
+        }
+        for (int i = 0; i < n; ++i) {
+            pos[ring.charAt(i) - 'a'].add(i);
+        }
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; ++i) {
+            Arrays.fill(dp[i], 0x3f3f3f);
+        }
+        for (int i : pos[key.charAt(0) - 'a']) {
+            dp[0][i] = Math.min(i, n - i) + 1;
+        }
+        for (int i = 1; i < m; ++i) {
+            for (int j : pos[key.charAt(i) - 'a']) {
+                for (int k : pos[key.charAt(i - 1) - 'a']) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][k] + Math.min(Math.abs(j - k), n - Math.abs(j - k)) + 1);
+                }
+            }
+        }
+        return Arrays.stream(dp[m - 1]).min().getAsInt();
+    }
+}
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/freedom-trail/solution/zi-you-zhi-lu-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
