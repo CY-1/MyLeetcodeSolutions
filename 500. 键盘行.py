2030c6863b05c6553953c244785c96302d51b566
Y.D.C <45694174+CY-1@users.noreply.github.com>
2020-11-24 10:07:02 +0800

    Create 500. 键盘行.py

---
+++ 500. 键盘行.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def findWords(self, words):
+        set1 = set('qwertyuiop')
+        set2 = set('asdfghjkl')
+        set3 = set('zxcvbnm')
+        res = []
+        for i in words:
+            x = i.lower()
+            setx = set(x)
+            if setx<=set1 or setx<=set2 or setx<=set3:
+                res.append(i)
+               
+            
+        return res
