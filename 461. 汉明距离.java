5a7e284148ca181ecefa9c10a8aa48c08c7016cb
Y.D.C <45694174+CY-1@users.noreply.github.com>
2020-11-27 09:32:17 +0800

    Create 461. 汉明距离.java

---
+++ 461. 汉明距离.java
@@ -0,0 +1,12 @@
+class Solution {
+  public int hammingDistance(int x, int y) {
+    int xor = x ^ y;
+    int distance = 0;
+    while (xor != 0) {
+      if (xor % 2 == 1)
+        distance += 1;
+      xor = xor >> 1;
+    }
+    return distance;
+  }
+}
