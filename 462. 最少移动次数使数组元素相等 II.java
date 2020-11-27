482d8e50713d07432f1b2ea5ba1cf17a1c509dff
Y.D.C <45694174+CY-1@users.noreply.github.com>
2020-11-27 09:21:02 +0800

    Update 462. 最少移动次数使数组元素相等 II.java

--- 462. 最少移动次数使数组元素相等 II.java
+++ 462. 最少移动次数使数组元素相等 II.java
@@ -8,12 +8,7 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             result=Math.abs(nums[mid]-nums[i])+result;
         }
-        int result2=0;
-        if(nums.length%2!=0){
-            for(int i=0;i<nums.length;i++){
-                result2=Math.abs(nums[mid]-nums[i])+result2;
-            }
-        }
-        return nums.length%2!=0?Math.min(result,result2):result;
+
+        return result;
     }
 }
