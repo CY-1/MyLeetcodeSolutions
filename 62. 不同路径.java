987e520da0fa6a6b80364d6e8b7862f9f3d6294d
Y.D.C <45694174+CY-1@users.noreply.github.com>
2020-12-09 09:07:21 +0800

    Create 62. 不同路径.java

---
+++ 62. 不同路径.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        long ans = 1;
+        for (int x = n, y = 1; y < m; ++x, ++y) {
+            ans = ans * x / y;
+        }
+        return (int) ans;
+    }
+}
+
