bfee10829494d1a26d59bdbf46e2e6453f7f272a
Y.D.C <45694174+CY-1@users.noreply.github.com>
2020-12-23 10:07:00 +0800

    Create 560. 和为K的子数组.java

---
+++ 560. 和为K的子数组.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        for (int start = 0; start < nums.length; ++start) {
+            int sum = 0;
+            for (int end = start; end >= 0; --end) {
+                sum += nums[end];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
+
