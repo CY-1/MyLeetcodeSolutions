3b1ebf018dae80a5e2ced534a9da277d12dac83b
Y.D.C <45694174+CY-1@users.noreply.github.com>
2020-12-27 09:54:34 +0800

    Create 205. 同构字符串.java

---
+++ 205. 同构字符串.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character,Character> map=new HashMap<>();
+        HashMap<Character,Character> map2=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i))&&map.get(s.charAt(i))!=t.charAt(i)){
+                return false;
+            }
+            else if(map2.containsKey(t.charAt(i))&&map2.get(t.charAt(i))!=s.charAt(i)){
+                return false;
+            }
+            else{
+                map.put(s.charAt(i),t.charAt(i));
+                map2.put(t.charAt(i),s.charAt(i));
+            }
+        }
+        return true;
+    }
+}
