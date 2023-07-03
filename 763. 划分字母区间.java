d0310fa5cdea2ce08f89e6da035241c6134a7ec0
C.D.Y <45694174+CY-1@users.noreply.github.com>
2023-07-03 09:17:57 +0800

    Update and rename 763. 划分字母区间 to 763. 划分字母区间.java

--- 763. 划分字母区间.java
+++ 763. 划分字母区间.java
@@ -18,7 +18,4 @@ class Solution {
     }
 }
 
-作者：LeetCode-Solution
-链接：https://leetcode-cn.com/problems/partition-labels/solution/hua-fen-zi-mu-qu-jian-by-leetcode-solution/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
